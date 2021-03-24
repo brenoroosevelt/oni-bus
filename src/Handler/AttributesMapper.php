91b643ed89368b36a148eec80c4cbd73a8e3a594
Breno Roosevelt <breno.jesus@ufms.br>
2021-03-24 15:02:06 -0400

    initial commit

--- src/Handler/AttributesMapper.php
+++ src/Handler/AttributesMapper.php
@@ -102,8 +102,9 @@ class AttributesMapper implements ClassMethodMapper
 
     protected function extractHandlerAttribute(ReflectionMethod $method): ?Handler
     {
-        $attribute = $method->getAttributes(Handler::class, ReflectionAttribute::IS_INSTANCEOF)[0] ?? null;
-        if ($attribute instanceof ReflectionAttribute) {
+        //$attribute = $method->getAttributes(Handler::class, ReflectionAttribute::IS_INSTANCEOF)[0] ?? null;
+        $attribute = $method->getAttributes(Handler::class)[0] ?? null;
+        if (!is_null($attribute)) {
             return $attribute->newInstance();
         }
 
