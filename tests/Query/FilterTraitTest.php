becdf782b7ee45f776f77d516b1135bbe5a7e30e
Breno Roosevelt <breno.jesus@ufms.br>
2021-03-30 09:10:04 -0400

    test FilterTrait

---
+++ tests/Query/FilterTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace OniBus\Test\Query;
+
+use OniBus\Query\Filter;
+use OniBus\Query\FilterTrait;
+use OniBus\Test\TestCase;
+
+class FilterTraitTest extends TestCase
+{
+    protected function newFilterTrait()
+    {
+        return new class {
+            use FilterTrait;
+        };
+    }
+
+    public function testShouldFilterTraitInitializeWithEmptyFilters()
+    {
+        $trait = $this->newFilterTrait();
+        $this->assertFalse($trait->hasFilters());
+    }
+
+    public function testShouldFilterTraitEmptyWhenFiltersAreEmpty()
+    {
+        $trait = $this->newFilterTrait();
+        $this->invokeMethod($trait, 'setFilters', [new Filter()]);
+        $this->assertFalse($trait->hasFilters());
+    }
+
+    public function testShouldFilterTraitSetFilter()
+    {
+        $trait = $this->newFilterTrait();
+        $this->invokeMethod($trait, 'setFilters', [new Filter(['field' => 'value'])]);
+        $this->assertEquals('value', $trait->filters()->get('field'));
+    }
+}
