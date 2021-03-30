36a6c52522b0972687f4356eeed1559e5f76ce67
Breno Roosevelt <breno.jesus@ufms.br>
2021-03-30 11:05:54 -0400

    test EventManager

--- tests/Event/EventManagerTest.php
+++ tests/Event/EventManagerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace OniBus\Test\Event;
 
 use OniBus\Event\EventManager;
+use OniBus\Event\EventProvider;
 use OniBus\Test\Fixture\GenericEvent;
 use OniBus\Test\TestCase;
 
@@ -14,6 +15,7 @@ class EventManagerTest extends TestCase
         $eventProvider = EventManager::instance();
         $eventProvider2 = EventManager::instance();
         $this->assertSame($eventProvider, $eventProvider2);
+        $this->assertInstanceOf(EventProvider::class, $eventProvider2);
     }
 
     public function testShouldEventManagerRecordAndReleaseEvents()
